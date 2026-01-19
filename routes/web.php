aa0f084f9fada808a51ea58bf560a2f7221dfe61
Ramonf96070 <ramonf96070@gmail.com>
2026-01-18 18:17:45 -0600

    Login
    Modificacion del login

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\dasboard\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('auth.login'); });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
